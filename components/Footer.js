47edcd187946fb09297c41020207e365aa00a0bf
sanoyusuke897 <sano@alivenet.com>
2023-02-17 11:25:19 +0900

    ♻️ Footer部分とLinkの部分をコンポーネント化

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css"
+import { Inter } from '@next/font/google'
+
+export function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Thanks by Vercel
+        </footer>
+    );
+}
