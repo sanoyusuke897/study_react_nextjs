a2b2bd90f9ca502e528233f31d2b3658b5c0d757
sanoyusuke897 <sano@alivenet.com>
2023-03-01 13:15:05 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,19 @@ import Head from 'next/head'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
+import { useCallback } from 'react'
+
+
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo)
+  }, []);
+
   return (
     <>
       <Head>
@@ -12,6 +23,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header />
+      <a 
+      href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index"/>
       <Footer />
     </>
